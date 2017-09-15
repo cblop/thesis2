284b2822865fafa94a433d670d65dbdf144cb24a
cblop <commanderblop@gmail.com>
2017-09-15 14:21:47 +0100

    initial

---
+++ auto/litrev.el
@@ -0,0 +1,19 @@
+(TeX-add-style-hook
+ "litrev"
+ (lambda ()
+   (LaTeX-add-labels
+    "cha:literature-review"
+    "sec:narratology"
+    "fig:vonnegut"
+    "fig:propp1"
+    "fig:propp2"
+    "sec:implementations"
+    "fig:rumelhart"
+    "fig:tspin"
+    "fig:mimesis"
+    "sec:model-logic"
+    "sec:emotional-models"
+    "sec:circumplex"
+    "fig:circumplex"))
+ :latex)
+
